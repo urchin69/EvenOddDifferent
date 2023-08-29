2f58c30e6f890164860f09058a3cba43a10aa233
urchin69 <urchin@onet.plgit config  --global user.email urchin@onet.plgit config  --global user.email urchin@onet.pl>
2023-08-29 22:28:15 +0200

    Add project files.

---
+++ EvenOddDifferent/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOddDifferent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę całkowitą, program określi czy jest liczba parzysta czy nieparzysta!");
+            var number = GetNumber();
+
+            if (number % 2 ==0 )
+            {
+                Console.WriteLine($" {number} to liczba parzysta.");
+            }
+            if (number % 2 != 0 )
+            {
+                Console.WriteLine($" {number} to liczba nieparzysta.");
+            }
+        }
+
+        private static int GetNumber()
+        {
+            while (true)
+            {           
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine(" Wpisany tekst nie jest liczbą całkowitą, wpisz liczbę całkowitą.");
+                    continue;
+                }
+                else
+                {
+                    return number;
+                }
+            }
+        }
+
+
+
+    }
+}
